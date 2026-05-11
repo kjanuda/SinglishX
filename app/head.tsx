58b99281fb616b22642d8c0fe502286b92696d3d
kjanuda <janudakodi@gmail.com>
2026-05-11 19:37:58 +0530

    first commit852222

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-adsense-account"
+        content="ca-pub-4481173940421216"
+      />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -88,12 +88,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="google-adsense-account"
-          content="ca-pub-4481173940421216"
-        />
-      </head>
       <body>
         <Script
           async
